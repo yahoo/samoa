b7645d055199f05c15330244f1c025cbcc98c356
ambodi <amir.rahnama@gmail.com>
2014-08-27 08:49:21 +0200

    update sentiment analysis task runner app

--- samoa-local/src/main/java/com/yahoo/labs/samoa/app.java
+++ samoa-local/src/main/java/com/yahoo/labs/samoa/app.java
@@ -6,6 +6,38 @@
  */
 package com.yahoo.labs.samoa;
 
+/*
+ * #%L
+ * SAMOA
+ * %%
+ * Copyright (C) 2013 - 2014 Yahoo! Inc.
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+import com.yahoo.labs.samoa.sentinel.task.SentimentAnalysis;
+import com.yahoo.labs.samoa.topology.impl.SimpleComponentFactory;
+
 public class app
 {
+    public static void main( String[] args ) {
+        SentimentAnalysis se = new SentimentAnalysis();
+        se.setFactory(new SimpleComponentFactory());
+
+        se.parallelismOption.setValue(4);
+        se.instanceLimitOption.setValue(100);
+
+        se.init();
+    }
 }
